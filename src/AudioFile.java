14d69ac57851dec4a7ae6eeb21ba6c6fa343181d
Florian Staudacher <florian_staudacher@yahoo.de>
2010-05-04 12:27:07 +0200

    added method stubs and placeholder file for playlist

--- src/AudioFile.java
+++ src/AudioFile.java
@@ -1,1 +1,69 @@
- 
+import java.net.URI; 
+
+/**
+ * This class contains the information to an audio file 
+ * 
+ * @author Florian Staudacher
+ *
+ */
+public class AudioFile {
+	
+	/**
+	 * declaration of the public variables  
+	 */
+	public String 	title;		// title of the track
+	public String 	artist;		// artist of the track
+	public String 	album;		// album of the track
+	public int 		length; 	// length in seconds
+	public URI 		path;		// path in the filesystem
+	
+	/**
+	 * constructor 
+	 * 
+	 * @param title
+	 * @param artist
+	 * @param album
+	 * @param length
+	 * @param path
+	 */
+	public AudioFile(String title, String artist, String album, int length, URI path) {
+		this.assignMemberVars(title, artist, album, length, path);
+	}
+	
+	/**
+	 * alternative constructor
+	 * 
+	 * @param title
+	 * @param artist
+	 * @param album
+	 * @param length
+	 * @param path
+	 */
+	public AudioFile(String title, String artist, String album, int length, String path) {
+		try {
+			URI tmpPath = new URI(path);
+			this.assignMemberVars(title, artist, album, length, tmpPath);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * assign the member variables
+	 * 
+	 * @param title
+	 * @param artist
+	 * @param album
+	 * @param length
+	 * @param path
+	 */
+	public void assignMemberVars(String title, String artist, String album, int length, URI path) {
+		this.title = title;
+		this.artist = artist;
+		this.album = album;
+		this.length = length;
+		this.path = path;
+	}
+
+}
