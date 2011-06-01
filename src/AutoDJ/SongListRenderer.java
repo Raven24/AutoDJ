776406546df9e6f82a8c0f508a1a74e6e3dcc99b
Christian Wurst <christian.wurst@gmail.com>
2011-06-01 03:33:56 +0200

    added javadoc

--- src/AutoDJ/SongListRenderer.java
+++ src/AutoDJ/SongListRenderer.java
@@ -1,3 +1,23 @@
+/**
+ * SongListRenderer.java
+ * (C) 2011 Florian Staudacher, Christian Wurst
+ * 
+ * This file is part of AutoDJ.
+ *
+ * AutoDJ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AutoDJ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AutoDJ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package AutoDJ;
 
 import java.awt.Component;
@@ -6,17 +26,27 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
+/**
+ * SongListRenderer is a subclass of JLabel and is used
+ * for formatting song information in the library and playlist
+ * view in JLists in AutoDJView.
+ * @see AutoDJView
+ */
+
 public class SongListRenderer extends JLabel implements ListCellRenderer {
 	
-    public SongListRenderer() {
-        setOpaque(true);
-    }
-	
-	
-	@Override
-	public Component getListCellRendererComponent(JList list, Object value,
+	/**
+	 * Creates a new SongListRenderer object.
+	 */
+	public SongListRenderer() {
+		setOpaque(true);
+	}
+
+    @Override
+    public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean cellHasFocus) {
 		
+    	// get the colors from the JList we "work for"
 		if(isSelected) {
 			setBackground(list.getSelectionBackground());
 			setForeground(list.getSelectionForeground());
